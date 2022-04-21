7bac6a3590707e4df0b2ed4f8b30296182e90784
Aung Ko Min <jonahaung@gmail.com>
2022-04-21 09:58:19 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MySpellCorrect
+
+A description of this package.
---
+++ Tests/MySpellCorrectTests/MySpellCorrectTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import MySpellCorrect
+
+final class MySpellCorrectTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(MySpellCorrect().text, "Hello, World!")
+    }
+}
